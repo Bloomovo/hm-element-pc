fd52c906f7d48cd3a89a38e38d87f1fb8b271d97
M.wait <18870742804@163.com>
2024-11-15 19:40:37 +0800

    实现element-ui按需引入和主题色定制

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import '@/utils/element.js'
+// 主题色定制
+import '@/styles/index.scss'
 
 Vue.config.productionTip = false
 
