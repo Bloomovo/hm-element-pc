687733338e9cd8c4687da04a8b38663cd311611b
M.wait <18870742804@163.com>
2024-11-16 09:19:03 +0800

    登录模块-样式美化

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,6 +1,20 @@
 <template>
-  <div class="app">
-    login
+  <div class="login-page">
+    <el-card class="el-card">
+      <template #header>黑马面经运营后台</template>
+      <el-form>
+        <el-form-item lable="用户名：">
+          <el-input placeholder="请输入用户名"></el-input>
+        </el-form-item>
+        <el-form-item lable="密码：">
+          <el-input placeholder="请输入密码："></el-input>
+        </el-form-item>
+        <el-from-item>
+          <el-button type="primary">登录</el-button>
+          <el-button>重置</el-button>
+        </el-from-item>
+      </el-form>
+    </el-card>
   </div>
 </template>
 
@@ -10,6 +24,29 @@ export default {
 }
 </script>
 
-<style lang="scss">
-
+<style lang="scss" scoped>
+.login-page {
+  min-height: 100vh;
+  // background: url(@/assets/login-bg.svg) no-repeat center / cover;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  .el-card {
+    width: 420px;
+    ::v-deep .el-card__header{
+      height: 80px;
+      background: rgba(114,124,245,1);
+      text-align: center;
+      line-height: 40px;
+      color: #fff;
+      font-size: 18px;
+    }
+  }
+  .el-form {
+    padding: 0 20px;
+  }
+  .tc {
+    text-align: center;
+  }
+}
 </style>
