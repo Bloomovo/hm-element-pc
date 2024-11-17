32cc2afd18ba6a4cb3f05c54a362c7159cfa5fcc
M.wait <18870742804@163.com>
2024-11-17 11:56:05 +0800

    登录访问拦截

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,6 @@
 import Vue from 'vue'
+import store from '@/store/index.js'
+import { Message } from 'element-ui'
 import VueRouter from 'vue-router'
 import Layout from '@/views/layout/index.vue'
 import Login from '@/views/login/index.vue'
@@ -22,6 +24,29 @@ const routes = [
 
 const router = new VueRouter({
   routes
+})
+
+// 白名单
+const whiteList = ['/login']
+
+// 路由导航守卫
+router.beforeEach((to, from, next) => {
+  // 是否存在 token
+  const token = store.state.user.token
+  if (token) {
+    next()
+    return
+  }
+
+  // 是否在白名单中
+  if (whiteList.includes(to.path)) {
+    next()
+    return
+  }
+
+  // 其它情况
+  Message.warning('登录后方可进入')
+  next('/login')
 })
 
 export default router
